193bfd820d706fa642915049daa0fc7d0735e84c
Alexandr <devshots.blog@gmail.com>
2022-01-10 15:38:26 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 export default function App() {
   return (
     <div>
-      <label>
-        Number
-        <input type="number" />;
-      </label>
-      <label>
-        Tel
-        <input type="tel" />;
-      </label>
+      <div>
+        <label>
+          Number
+          <input type="number" />;
+        </label>
+      </div>
     </div>
   );
 }
